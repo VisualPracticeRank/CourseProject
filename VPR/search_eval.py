16825a99d8b80fd783a657d6052eaa065d0f0fb4
mrente5 <mrente5@illinois.edu>
2020-11-29 00:55:05 -0600

    added bootstrap via crispy forms, added model view; delete; update. started metapy data spec

--- VPR/search_eval.py
+++ VPR/search_eval.py
@@ -8,6 +8,35 @@ import os
 def load_ranker(cfg_file):
     return metapy.index.OkapiBM25()
 
+def return_score_data(idx, query):
+    avg_dl = idx.avg_dog_length()
+    num_docs = idx.num_docs()
+    total_terms = idx.total_corpus_terms()
+    query_length = len(query)
+    t_id = 0
+    query_term_weight = 0
+    doc_count = idx.doc_freq(t_id)
+    corpus_term_count = idx.total_num_occurences(t_id)
+    # Document
+    d_id = 0
+    doc_term_count = 0
+    doc_size = 0
+    doc_unique_terms = 0
+
+def create_inverted_index(file):
+    #Generate tmp config file
+
+    #Make Inverted Index
+
+
+    #Compress Inverted index
+
+    #Pass Path to Compressed Index
+    return 1
+
+def load_inverted_index(index_as_string):
+    return 1
+
 def run_query(q):
     cfg = "config.toml"
     idx = metapy.index.make_inverted_index(cfg)
