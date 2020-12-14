ea71819861e97ede7d9fb6ac51034c7ec01f1e52
mrente5 <mrente5@illinois.edu>
2020-12-13 18:48:25 -0600

    extra doc and corpus data added

--- VPR/webui/models.py
+++ VPR/webui/models.py
@@ -18,6 +18,10 @@ class Dataset(models.Model):
     data = models.FileField(upload_to=dataset_id_path)
     qrels = models.FileField(upload_to=qrels_id_path)
     queries = models.FileField(upload_to=queries_id_path)
+    unique_terms = models.IntegerField(null=True)
+    avg_dl =  models.FloatField(null=True)
+    num_docs = models.IntegerField(null=True)
+
 
     def __str__(self):
         return self.name
@@ -25,6 +29,8 @@ class Dataset(models.Model):
 class Document(models.Model):
     document_id = models.IntegerField()
     body = models.TextField()
+    doc_size = models.IntegerField(null=True)
+    doc_unique_terms = models.IntegerField(null=True)
     dataset = models.ForeignKey(
         'Dataset',
         on_delete=models.CASCADE,
